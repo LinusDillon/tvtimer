3d8be87bad9a1111dbb823feb18912f6d6bf4dd4
Linus Dillon <linus.dillon@gmail.com>
2019-05-10 22:22:52 +1000

    Calculates TV on time

--- tvtimer.py
+++ tvtimer.py
@@ -1,46 +1,74 @@
 import paho.mqtt.client as mqtt
+import time
+import json
 
 
-class TvTimerDaemon(Daemon):
+class TvTimerDaemon():
 
-    def __init__(self, mqttServer, mqttPort, switchName):
+    def __init__(self, mqttServer, mqttPort, switchName, powerThreshold):
         self.mqttServer = mqttServer
         self.mqttPort = mqttPort
         self.switchName = switchName
+        self.powerThreshold = powerThreshold
         self.sensorTopic = "tele/" + self.switchName + "/SENSOR"
+        self.lastTvPowerOnState = False
+        self.tvPowerOnTime = time.time()
+        self.tvPowerOffTime = time.time()
+        self.totalOnTimeToday = 0
+        self.totalOnTimeTodayWhenPoweredOn = 0
         self.mqttClient = mqtt.Client()
         self.mqttClient.on_connect = self.mqttOnConnect
         self.mqttClient.on_message = self.mqttOnMessage
-    
-    def mqttOnConnect(client, userdata, flags, rc):
+
+    def mqttOnConnect(self, client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
         print("Subscribing to " + self.sensorTopic)
         client.subscribe(self.sensorTopic)
 
-    def mqttOnMessage(client, userdata, msg):
+    def mqttOnMessage(self, client, userdata, msg):
         print(msg.topic + " " + str(msg.payload))
+        if msg.topic == self.sensorTopic:
+            # Extract the values we are interested in
+            jsonPayload = str(msg.payload.decode("utf-8", "ignore"))
+            objectPayload = json.loads(jsonPayload)
+            power = objectPayload["ENERGY"]["Power"]
+            print("TV power use is " + str(power))
+            tvPowerOnState = (power > self.powerThreshold)
+            if tvPowerOnState and not self.lastTvPowerOnState:
+                # TV has been turned on
+                self.tvPowerOnTime = time.time()
+                self.totalOnTimeTodayWhenPoweredOn = self.totalOnTimeToday
+            elif not tvPowerOnState and self.lastTvPowerOnState:
+                # TV has been turned off
+                self.tvPowerOffTime = time.time()
+            self.lastTvPowerOnState = tvPowerOnState
 
     def run(self):
         self.mqttClient.connect(self.mqttServer, self.mqttPort, 60)
         self.mqttClient.loop_start()
+        loopDivisor = 0
         while True:
-            time.sleep(30)
+            # If TV is on, update the total on time
+            if self.lastTvPowerOnState:
+                elapsedOnTime = time.time() - self.tvPowerOnTime
+                self.totalOnTimeToday = self.totalOnTimeTodayWhenPoweredOn + elapsedOnTime
+            if (loopDivisor % 60) == 0:
+                print("TV on time today is " + str(self.totalOnTimeToday))
+            loopDivisor += 1
+            time.sleep(1)
         self.mqttClient.loop_stop()
 
+# TODO
+# Push total on time today to topic
+# Push day to topic
+# Read total time today + day from topic and apply to self.totalOnTimeToday if for same day
+# Reset total time today/day at configured time each day
+# Calculate week+total time this week/push to topic/read from topic on startup if same week
+# Read default and applied limits from topic
+# Switch off TV if reached applied limits
+# Read limit bypass codes from topic and apply
+# Reset applied limits to default limits each day
 
 if __name__ == "__main__":
-    daemon = TvTimerDaemon()
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.run()
-        elif 'stop' == sys.argv[1]:
-            print "Not implemented"
-        elif 'restart' == sys.argv[1]:
-            print "Not implemented"
-        else:
-            print "Unknown command"
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print "usage: %s start|stop|restart" % sys.argv[0]
-        sys.exit(2)
+    daemon = TvTimerDaemon("zen", 1883, "sonoff-tv", 20)
+    daemon.run()
